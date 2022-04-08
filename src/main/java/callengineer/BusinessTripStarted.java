ecbdd2e5da066f8e7febd27da37b60388bd72cb5
YONGSOO BYUN <yongdoo@hanwha.com>
2022-04-08 14:11:49 +0900

    test

--- src/main/java/callengineer/BusinessTripStarted.java
+++ src/main/java/callengineer/BusinessTripStarted.java
@@ -15,21 +15,21 @@ public class BusinessTripStarted extends AbstractEvent {
         return id;
     }
 
-    public void setId(Long Id) {
+    public void setId(Long id) {
         this.id = id;
     }
     public Long getCallId() {
         return callId;
     }
 
-    public void setCallId(Long CallId) {
+    public void setCallId(Long callId) {
         this.callId = callId;
     }
     public String getStatus() {
         return status;
     }
 
-    public void setStatus(String Status) {
+    public void setStatus(String status) {
         this.status = status;
     }
 }
